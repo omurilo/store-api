ef2b4f4bae993cb60afe15b99163ceb84482978d
Murilo Henrique <hi@omurilo.dev>
2021-07-08 23:35:06 -0300

    fix(api): :ambulance: enable cors header

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 import { EntityNotFoundExceptionFilter } from './expection-filters/entity-not-found.exception-filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalFilters(new EntityNotFoundExceptionFilter());
   app.useGlobalPipes(new ValidationPipe({ errorHttpStatusCode: 422 }));
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
